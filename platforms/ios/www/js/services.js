07abc29a927f6b4b9699ea1dbc0e9af9c0dda81d
Jimmy Hedström <jimmy.hedstrom@hotmail.com>
2016-01-08 01:54:26 +0100

    Vibration and barcode scanner

---
+++ platforms/ios/www/js/services.js
@@ -0,0 +1,18 @@
+angular.module('starter.services', [])
+
+.factory('DBService', function($http, $ionicPopup) {
+  return {
+    sendToDB: function (json, showPopup) {
+      var promise = $http({method:"POST", data:json, url:"http://userapan.myds.me/streckbase2_server/index.php"})
+        .success(function (data, status, headers, config) {
+          if (data.success !== 1) {
+            console.log(data.error_msg);
+          }
+        })
+        .error(function (data, status, headers, config) {
+          console.log("Error in DBService");
+        });
+      return promise;
+    }
+  };
+});
