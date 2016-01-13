62a4301eb9d01aa9dfb9e4c6ed4a4cd2fd633ba4
Jimmy Hedström <jimmy.hedstrom@hotmail.com>
2016-01-13 11:26:25 +0100

    user_id is now correct

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -149,42 +149,36 @@ angular.module('starter.controllers', ['angularMoment', 'ngCordova', 'nvd3', 'ng
 })
 
 .controller('FeedCtrl', function($scope, ionicMaterialMotion, ionicMaterialInk, DBService) {
-
-    function getColor() {
-        var color = ['green.jpg', 'blue.jpg', 'purple.jpg'];
-        color = color[Math.floor(Math.random()*color.length)];
-        return "../img/" + color;
-    }
+  function getColor() {
+    var color = ['green.jpg', 'blue.jpg', 'purple.jpg'];
+    color = color[Math.floor(Math.random()*color.length)];
+    return "../img/" + color;
+  }
       
-    var sendData = {'tag':'getFeed'};
-    $scope.doRefresh = function() {
-        DBService.sendToDB(sendData, false).then(function(promise) {
-          if (promise.data.success === 1) {
-             $scope.feed = promise.data.feed;
-             angular.forEach($scope.feed, function(c) {
-                c.date = new Date(c.date*1000);
-                c.datediff = moment(c.date).diff(moment(new Date), 'days');
-                if (!c.image)
-                    c.image = getColor();
-            }); 
+  var sendData = {'tag':'getFeed'};
+  $scope.doRefresh = function() {
+    DBService.sendToDB(sendData, false).then(function(promise) {
+      if (promise.data.success === 1) {
+        $scope.feed = promise.data.feed;
+        angular.forEach($scope.feed, function(c) {
+          c.date = new Date(c.date*1000);
+          c.datediff = moment(c.date).diff(moment(new Date), 'days');
+          if (!c.image) {
+            c.image = getColor();
           }
-        }).finally(function() {
-          // Stop the ion-refresher from spinning
-          $scope.$broadcast('scroll.refreshComplete');
-        });
-      };
+        }); 
+      }
+    }).finally(function() {
+      // Stop the ion-refresher from spinning
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
 
-        // Update feed when user enters scene
-        $scope.$on('$ionicView.loaded', function(){
-            $scope.doRefresh();
-        });
-
-    ionicMaterialMotion.blinds();
-
-    // // Activate ink for controller
-    // ionicMaterialInk.displayEffect();
-    
-
+  // Update feed when user enters scene
+  $scope.$on('$ionicView.loaded', function(){
+      $scope.doRefresh();
+  });
+  ionicMaterialMotion.blinds();
 })
 
 .controller('ViewCommentsCtrl', function($scope, $stateParams, DBService) {
@@ -208,7 +202,7 @@ angular.module('starter.controllers', ['angularMoment', 'ngCordova', 'nvd3', 'ng
         if (!count) {
             count = 1;
         }
-        var sendData = {'tag':'purchaseItem', 'user_id': '1', 'item_id':item, 'count':count};
+        var sendData = {'tag':'purchaseItem', 'user_id': window.localStorage['user_id'], 'item_id':item, 'count':count};
         DBService.sendToDB(sendData, false).then(function(promise) {
             if (promise.data.success === 1) {
                 if (window.cordova) {
