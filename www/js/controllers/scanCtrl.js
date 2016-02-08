5f4d226b38a3395eb17e3d5bcbd68266aa06eebf
Jimmy Hedström <jimmy.hedstrom@hotmail.com>
2016-02-08 21:32:43 +0100

    Barcode scanner now works again

--- www/js/controllers/scanCtrl.js
+++ www/js/controllers/scanCtrl.js
@@ -1,6 +1,10 @@
 var app = angular.module('starter.controllers');
 
 app.controller('ScanCtrl', function($scope, $ionicPlatform, $ionicHistory, $state, DBService, $cordovaVibration, $cordovaFlashlight) {
+  $scope.$on('$ionicView.enter', function(){
+    $scope.openScanner();
+  });
+
   $scope.buy = function(barcode) {
     var sendData = {'tag':'purchaseBarcodeItem', 'user_id':window.localStorage['user_id'], 'barcode':barcode};        
 
@@ -18,38 +22,40 @@ app.controller('ScanCtrl', function($scope, $ionicPlatform, $ionicHistory, $stat
     });
   }
 
-  if (window.cordova) {
-    var avail;    
-    $ionicPlatform.ready(function() {      
-      $cordovaFlashlight.available().then(function(availability) {
-        avail = availability; // is available
-        $cordovaFlashlight.switchOn();
-      }, function () {
-        avail = false;
-      }); 
+  $scope.openScanner = function() {
+    if (window.cordova) {
+      var avail;    
+      $ionicPlatform.ready(function() {      
+        $cordovaFlashlight.available().then(function(availability) {
+          avail = availability; // is available
+          $cordovaFlashlight.switchOn();
+        }, function () {
+          avail = false;
+        }); 
 
-      cordova.plugins.barcodeScanner.scan(
-        function (result) {
-          if (result.cancelled) {
+        cordova.plugins.barcodeScanner.scan(
+          function (result) {
+            if (result.cancelled) {
+              $ionicHistory.goBack(-1);
+            } else {
+              $scope.buy(result.text);
+            }
+            if (avail) {
+              $cordovaFlashlight.switchOff();
+            }          
+          }, 
+          function (error) {
+            //alert("Scanning failed: " + error);
+            console.log("Issue with barcode scanner within app");
+            if (avail) {
+              $cordovaFlashlight.switchOff();
+            }          
             $ionicHistory.goBack(-1);
-          } else {
-            $scope.buy(result.text);
           }
-          if (avail) {
-            $cordovaFlashlight.switchOff();
-          }          
-        }, 
-        function (error) {
-          //alert("Scanning failed: " + error);
-          console.log("Issue with barcode scanner within app");
-          if (avail) {
-            $cordovaFlashlight.switchOff();
-          }          
-          $ionicHistory.goBack(-1);
-        }
-      );
-    });        
-  } else {
-    $scope.infoText = "Denna funktion fungerar ej i webbläsaren.";
+        );
+      });        
+    } else {
+      $scope.infoText = "Denna funktion fungerar ej i webbläsaren.";
+    }
   }
 });
